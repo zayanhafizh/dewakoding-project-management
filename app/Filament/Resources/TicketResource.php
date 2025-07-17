eade431184637c23c8b612a4b3343628c8a7e00a
Dewakoding <septiawanajipradana@gmail.com>
2025-07-17 08:34:52 +0700

    fix: resolved bug causing missing view page from resource

--- app/Filament/Resources/TicketResource.php
+++ app/Filament/Resources/TicketResource.php
@@ -431,6 +431,7 @@ class TicketResource extends Resource
         return [
             'index' => Pages\ListTickets::route('/'),
             'create' => Pages\CreateTicket::route('/create'),
+            'view' => Pages\ViewTicket::route('/{record}'),
             'edit' => Pages\EditTicket::route('/{record}/edit'),
         ];
     }
